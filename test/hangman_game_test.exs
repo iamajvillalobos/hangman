160e0cfe342e7a8a2ee12d46cf66178b2d80d2b2
Arman Jon Villalobos <me@armanjon.com>
2017-08-12 10:58:35 +0800

    Add test for Hangman.Game#new_game/0 that all letters are downcase

--- test/hangman_game_test.exs
+++ test/hangman_game_test.exs
@@ -10,4 +10,14 @@ defmodule HangmanGameTest do
     assert game.game_state == :initializing
     assert length(game.letters) > 0
   end
+
+  test "new_game returns lowercase letters to guess" do
+    game = Game.new_game
+
+    result = game.letters
+              |> Enum.map(&(String.downcase(&1) == &1))
+              |> Enum.all?(fn(_x) -> true end)
+
+    assert result == true
+  end
 end
